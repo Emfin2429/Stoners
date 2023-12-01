14cfcb09b46078b3d93ff5adf96915275f30a273
Emma <90479062+Emfin2429@users.noreply.github.com>
2023-12-01 10:01:26 -0700

    Hella Comments

--- ClassifyModel.py
+++ ClassifyModel.py
@@ -1,13 +1,16 @@
-import tensorflow as tf
-from tensorflow import keras
-import requests
-requests.packages.urllib3.disable_warnings()
-import ssl
-from FGSA import fgsm_attack
-import numpy as np
-import random
-import matplotlib.pyplot as plt
+# Import necessary libraries
+import tensorflow as tf  # TensorFlow is an open-source machine learning framework
+from tensorflow import keras  # Keras is an API for building and training deep learning models
+import requests  # Requests is a library for making HTTP requests in Python
+requests.packages.urllib3.disable_warnings()  # Disable SSL warnings in requests
+import ssl  # SSL (Secure Sockets Layer) is a protocol for secure communication over a computer network
+from FGSA import fgsm_attack  # Import the Fast Gradient Sign Method (FGSM) attack function from a custom module
+import numpy as np  # NumPy is a library for numerical operations in Python
+import random  # Python library for generating random numbers
+import matplotlib.pyplot as plt  # Matplotlib is a plotting library for Python
 
+
+# This code is to address an error in calls for the Mac Book
 try:
     _create_unverified_https_context = ssl._create_unverified_context
 except AttributeError:
@@ -24,37 +27,43 @@ mnist = tf.keras.datasets.mnist
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 
-# Preprocess the data
+# Preprocess the data by scaling pixel values to the range [0, 1]
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-# Build the model
+
+# Create a sequential model, which is a linear stack of layers
 model = keras.Sequential([
+    # Convolutional layer with 32 filters, each of size (3, 3), using ReLU activation
     keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    
+    # Max pooling layer with a pool size of (2, 2)
     keras.layers.MaxPooling2D(2, 2),
+    
+    # Convolutional layer with 64 filters, each of size (3, 3), using ReLU activation
     keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    
+    # Max pooling layer with a pool size of (2, 2)
     keras.layers.MaxPooling2D(2, 2),
+    
+    # Flatten layer to convert the 2D matrix data to a vector for the fully connected layers
     keras.layers.Flatten(),
+    
+    # Dense (fully connected) layer with 128 neurons and ReLU activation
     keras.layers.Dense(128, activation='relu'),
+    
+    # Dense (fully connected) layer with 10 neurons (output layer for classification) and softmax activation
     keras.layers.Dense(10, activation='softmax')
 ])
 
-# Compile the model
+
+# Compile the model with an optimizer, loss function, and evaluation metric
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
 # Train the model
 model.fit(train_images.reshape(-1, 28, 28, 1), train_labels, epochs=5)
-
-
-
-# Choose a random test image and label
-"""
-index = np.random.randint(0, len(test_images))
-image = test_images[index]
-label = np.array([test_labels[index]])
-"""
 
 # Evaluate the model on the original and perturbed images
 perturbed_test_images = test_images.copy()
@@ -80,22 +89,6 @@ for index in range(len(perturbed_test_images)):
         #replaces OG image with new perturbed image
         perturbed_test_images[index] = perturbed_image.squeeze()
 
-
-"""
-        for i in range(1, 100):
-            epsilon = i / 100.0
-            perturbed_image = fgsm_attack(model, imageToMutate.reshape(1, 28, 28, 1), label, epsilon)
-            new_Perb_image = np.argmax(model.predict(perturbed_image))
-            og_image = np.argmax(original_pred)
-
-            if new_Perb_image != og_image:
-                final_perturbed_image = perturbed_image
-                final_epsilon = epsilon
-                listOfEpsilons.append(final_epsilon)
-                numChanged += 1 
-                perturbed_test_images[index] = final_perturbed_image.squeeze()
-                break
-"""
 #test the accuracy of og data set
 test_loss, test_acc = model.evaluate(test_images.reshape(-1, 28, 28, 1), test_labels)
 print("Test accuracy of OG File:", test_acc)
@@ -104,44 +97,14 @@ print("Test accuracy of OG File:", test_acc)
 test_loss2, test_acc2 = model.evaluate(perturbed_test_images.reshape(-1, 28, 28, 1), perb_test_labels)
 print("Test accuracy of changed File:", test_acc2)
 
-# Code for testing a single image with a fast gradient sign attack
-"""
-# Evaluate the model on the original and perturbed images
-original_pred = model.predict(image.reshape(1, 28, 28, 1))
-final_perturbed_image = 0
-final_epsilon = 0
-
-for i in range(1, 100):
-    epsilon = i / 100.0
-    perturbed_image = fgsm_attack(model, image.reshape(1, 28, 28, 1), label, epsilon)
-    new_Perb_image = np.argmax(model.predict(perturbed_image))
-    og_image = np.argmax(original_pred)
-
-    if new_Perb_image != og_image:
-        final_perturbed_image = perturbed_image
-        final_epsilon = epsilon
-        break
-
-# Evaluate the model on the original and perturbed images
-perturbed_pred = model.predict(final_perturbed_image)
-
-# Display results
-print("Original Prediction:", np.argmax(original_pred))
-print("Perturbed Prediction:", np.argmax(perturbed_pred ))
-print("Final epsilon: ", final_epsilon)
-
-#print("Number of training samples:", len(train_images))
-#print("Number of testing samples:", len(test_images))
-"""
-# Choose a random test image and label
-"""
+#Code to visually inspect what we just did
+# This gets the manipulated Image and label
 image = perturbed_test_images[DisplayIndex]
 label = np.array([perb_test_labels[DisplayIndex]])
 
-# Evaluate the model on the perturbed image
+# Tests to see how the model does on the manipulated image
 perturbed_pred = model.predict(image.reshape(1, 28, 28, 1))
 predicted_label = np.argmax(perturbed_pred)
-
 
 # Display the original image
 plt.imshow(test_images[DisplayIndex], cmap='gray')
@@ -152,4 +115,4 @@ plt.show()
 plt.imshow(image, cmap='gray')
 plt.title(f"Perturbed MNIST Digit: {predicted_label}")
 plt.show()
-"""
+
--- FGSA.py
+++ FGSA.py
@@ -1,47 +1,32 @@
-import numpy as np
-import tensorflow as tf
-from tensorflow import keras
-import requests
-requests.packages.urllib3.disable_warnings()
-import ssl
+# Import necessary libraries
+import numpy as np  # NumPy is a library for numerical operations in Python
+import tensorflow as tf  # TensorFlow is an open-source machine learning framework
+from tensorflow import keras  # Keras is an API for building and training deep learning models
+import requests  # Requests is a library for making HTTP requests in Python
+requests.packages.urllib3.disable_warnings()  # Disable SSL warnings in requests
+import ssl  # SSL (Secure Sockets Layer) is a protocol for secure communication over a computer network
 
+# Define a function for generating adversarial examples using the Fast Gradient Sign Method (FGSM)
 def fgsm_attack(model, images, labels, epsilon=0.01):
-    # Set the model to trainable
+    # Set the model to not be trainable during the attack
     model.trainable = False
     
     # Use GradientTape to compute the gradient of the loss with respect to the input images
     with tf.GradientTape() as tape:
+        # Convert input images to TensorFlow tensors of type float32
         images = tf.convert_to_tensor(images, dtype=tf.float32)
-        tape.watch(images)
-        predictions = model(images)
-        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)
+        tape.watch(images)  # Watch the input images for computing gradients
+        predictions = model(images)  # Obtain model predictions for the input images
+        loss = tf.keras.losses.sparse_categorical_crossentropy(labels, predictions)  # Compute the cross-entropy loss
 
-    # Get the gradient of the loss with respect to the input
+    # Get the gradient of the loss with respect to the input images
     gradient = tape.gradient(loss, images)
 
-    # Generate adversarial examples by adding epsilon times the sign of the gradient to the input
+    # Generate adversarial examples by adding epsilon times the sign of the gradient to the input images
     perturbed_images = images + epsilon * tf.sign(gradient)
 
-    # Clip the perturbed images to ensure they are within the valid range [0, 1]
+    # Clip the perturbed images to ensure they are within the valid pixel value range [0, 1]
     perturbed_images = tf.clip_by_value(perturbed_images, 0, 1)
 
+    # Convert the TensorFlow tensor to a NumPy array and return the perturbed images
     return perturbed_images.numpy()
-
-"""
-# Choose a random test image and label
-index = np.random.randint(0, len(test_images))
-image = test_images[index]
-label = np.array([test_labels[index]])
-
-# Generate adversarial example
-epsilon = 0.01
-perturbed_image = fgsm_attack(model, image.reshape(1, 28, 28, 1), label, epsilon)
-
-# Evaluate the model on the original and perturbed images
-original_pred = model.predict(image.reshape(1, 28, 28, 1))
-perturbed_pred = model.predict(perturbed_image)
-
-# Display results
-print("Original Prediction:", np.argmax(original_pred))
-print("Perturbed Prediction:", np.argmax(perturbed_pred))
-"""
